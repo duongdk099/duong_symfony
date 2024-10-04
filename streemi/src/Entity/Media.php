fb4ca25230b430cd6a702016b38a7e5382902253
duongd <duongdk69@gmail.com>
2024-10-04 17:03:44 +0200

    DO entites

--- streemi/src/Entity/Media.php
+++ streemi/src/Entity/Media.php
@@ -2,12 +2,20 @@
 
 namespace App\Entity;
 
+use App\Enum\MediaTypeEnum;
 use App\Repository\MediaRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\InheritanceType;
+use Doctrine\ORM\Mapping\DiscriminatorColumn;
+use Doctrine\ORM\Mapping\DiscriminatorMap;
 
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
+#[ORM\InheritanceType('SINGLE_TABLE')]  // This tells Doctrine to store both Movie and Serie in one table.
+#[ORM\DiscriminatorColumn(name: 'media_type', type: 'string')]
+#[ORM\DiscriminatorMap(['movie' => Movie::class, 'serie' => Serie::class])]
 class Media
 {
     #[ORM\Id]
@@ -32,6 +40,30 @@ class Media
      */
     #[ORM\ManyToMany(targetEntity: Language::class, inversedBy: 'media')]
     private Collection $languages;
+
+    #[ORM\Column(enumType: MediaTypeEnum::class)]
+    private ?MediaTypeEnum $media_type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $short_description = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $long_description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $release_date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cover_image = null;
+
+    #[ORM\Column]
+    private array $staff = [];
+
+    #[ORM\Column]
+    private array $casts = [];
 
     public function __construct()
     {
@@ -116,6 +148,102 @@ class Media
     public function removeLanguage(Language $language): static
     {
         $this->languages->removeElement($language);
+
+        return $this;
+    }
+
+    public function getMediaType(): ?MediaTypeEnum
+    {
+        return $this->media_type;
+    }
+
+    public function setMediaType(MediaTypeEnum $media_type): static
+    {
+        $this->media_type = $media_type;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->short_description;
+    }
+
+    public function setShortDescription(string $short_description): static
+    {
+        $this->short_description = $short_description;
+
+        return $this;
+    }
+
+    public function getLongDescription(): ?string
+    {
+        return $this->long_description;
+    }
+
+    public function setLongDescription(string $long_description): static
+    {
+        $this->long_description = $long_description;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->release_date;
+    }
+
+    public function setReleaseDate(\DateTimeInterface $release_date): static
+    {
+        $this->release_date = $release_date;
+
+        return $this;
+    }
+
+    public function getCoverImage(): ?string
+    {
+        return $this->cover_image;
+    }
+
+    public function setCoverImage(string $cover_image): static
+    {
+        $this->cover_image = $cover_image;
+
+        return $this;
+    }
+
+    public function getStaff(): array
+    {
+        return $this->staff;
+    }
+
+    public function setStaff(array $staff): static
+    {
+        $this->staff = $staff;
+
+        return $this;
+    }
+
+    public function getCasts(): array
+    {
+        return $this->casts;
+    }
+
+    public function setCasts(array $casts): static
+    {
+        $this->casts = $casts;
 
         return $this;
     }
---
+++ streemi/src/Entity/Serie.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SerieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SerieRepository::class)]
+class Serie extends Media
+{
+    
+}
